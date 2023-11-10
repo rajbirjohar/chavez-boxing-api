39719846d18c59c9c0d77dbec3dec1cd4ed8ce26
rajbirjohar <rajbirjohar1@gmail.com>
2023-11-09 19:09:21 -0700

    initial commit

---
+++ src/collections/Recipes.ts
@@ -0,0 +1,55 @@
+import { CollectionConfig } from "payload/types";
+import {
+  HTMLConverterFeature,
+  lexicalEditor,
+  lexicalHTML,
+} from "@payloadcms/richtext-lexical";
+
+const Recipes: CollectionConfig = {
+  access: {
+    read: () => true,
+  },
+  slug: "recipes",
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "description",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "ingredients",
+      type: "richText",
+      required: true,
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          HTMLConverterFeature({}),
+        ],
+      }),
+    },
+    {
+      name: "instructions",
+      type: "richText",
+      required: true,
+      editor: lexicalEditor({
+        features: ({ defaultFeatures }) => [
+          ...defaultFeatures,
+          HTMLConverterFeature({}),
+        ],
+      }),
+    },
+    lexicalHTML("ingredients", {
+      name: "ingredients_html",
+    }),
+    lexicalHTML("instructions", {
+      name: "instructions_html",
+    }),
+  ],
+};
+
+export default Recipes;
---
+++ src/payload.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+
+import { payloadCloud } from "@payloadcms/plugin-cloud";
+import { mongooseAdapter } from "@payloadcms/db-mongodb";
+import { webpackBundler } from "@payloadcms/bundler-webpack";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import { buildConfig } from "payload/config";
+
+import Users from "./collections/Users";
+import Recipes from "./collections/Recipes";
+
+export default buildConfig({
+  admin: {
+    user: Users.slug,
+    bundler: webpackBundler(),
+  },
+  editor: lexicalEditor({}),
+  collections: [Users, Recipes],
+  cors: ["http://localhost:4321"],
+  typescript: {
+    outputFile: path.resolve(__dirname, "payload-types.ts"),
+  },
+  graphQL: {
+    schemaOutputFile: path.resolve(__dirname, "generated-schema.graphql"),
+  },
+  plugins: [payloadCloud()],
+  db: mongooseAdapter({
+    url: process.env.DATABASE_URI,
+  }),
+});
